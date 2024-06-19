1c66cc9742ba0b141894d7d22abe54e5c489450c
Kelvin <121232509+kelvinyunan@users.noreply.github.com>
2024-06-20 04:09:20 +0700

    creating DOM reference for buttons

---
+++ javascript.js
@@ -0,0 +1,28 @@
+const oneBtn = document.getElementById('oneBtn');
+const twoBtn = document.getElementById('twoBtn');
+const threeBtn = document.getElementById('threeBtn');
+const fourBtn = document.getElementById('fourBtn');
+const fiveBtn = document.getElementById('fiveBtn');
+const sixBtn = document.getElementById('sixBtn');
+const sevenBtn = document.getElementById('sevenBtn');
+const eightBtn = document.getElementById('eightBtn');
+const nineBtn = document.getElementById('nineBtn');
+const zeroBtn = document.getElementById('zeroBtn');
+const clearBtn = document.getElementById('clearBtn');
+const negativeBtn = document.getElementById('negativeBtn');
+const percentBtn = document.getElementById('percentBtn');
+const multiplyBtn = document.getElementById('multiplyBtn');
+const minusBtn = document.getElementById('minusBtn');
+const divideBtn = document.getElementById('divideBtn');
+const plusBtn = document.getElementById('plusBtn');
+const equalBtn = document.getElementById('equalBtn');
+const decimalBtn = document.getElementById('decimalBtn');
+const screenNum = document.getElementById('screenNum');
+
+let firstOperand = "";
+let secondOperand = "";
+let operator = "";
+
+oneBtn.addEventListener('click',()=>{
+    firstOperand
+})
