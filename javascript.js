becb25fb3b3de3e79c41825dd56f15fb89286fd6
Kelvin <121232509+kelvinyunan@users.noreply.github.com>
2024-06-21 04:50:27 +0700

    calculation working, next is the blue buttons

--- javascript.js
+++ javascript.js
@@ -1,28 +1,73 @@
-const oneBtn = document.getElementById('oneBtn');
-const twoBtn = document.getElementById('twoBtn');
-const threeBtn = document.getElementById('threeBtn');
-const fourBtn = document.getElementById('fourBtn');
-const fiveBtn = document.getElementById('fiveBtn');
-const sixBtn = document.getElementById('sixBtn');
-const sevenBtn = document.getElementById('sevenBtn');
-const eightBtn = document.getElementById('eightBtn');
-const nineBtn = document.getElementById('nineBtn');
-const zeroBtn = document.getElementById('zeroBtn');
+const numBtn = document.querySelectorAll('.operand')
+const operatorBtn = document.querySelectorAll('.operator')
 const clearBtn = document.getElementById('clearBtn');
 const negativeBtn = document.getElementById('negativeBtn');
 const percentBtn = document.getElementById('percentBtn');
-const multiplyBtn = document.getElementById('multiplyBtn');
-const minusBtn = document.getElementById('minusBtn');
-const divideBtn = document.getElementById('divideBtn');
-const plusBtn = document.getElementById('plusBtn');
 const equalBtn = document.getElementById('equalBtn');
 const decimalBtn = document.getElementById('decimalBtn');
-const screenNum = document.getElementById('screenNum');
+const screenNum = document.getElementById('screen');
 
 let firstOperand = "";
 let secondOperand = "";
 let operator = "";
 
-oneBtn.addEventListener('click',()=>{
-    firstOperand
+numBtn.forEach((button)=>{
+    button.addEventListener('click',()=>{
+
+        if (firstOperand == "") {
+            firstOperand = button.value.toString();
+            screenNum.textContent = firstOperand;
+        } else if (firstOperand != null && operator == "") {
+            firstOperand += button.value.toString();
+            screenNum.textContent = firstOperand;
+        } else if (secondOperand == "" && operator != "") {
+            secondOperand = button.value.toString();
+            screenNum.textContent = secondOperand;
+        } else {
+            secondOperand += button.value.toString();
+            screenNum.textContent = secondOperand;
+        }
+
+    })
+})
+
+operatorBtn.forEach((button)=>{
+    button.addEventListener('click',()=>{
+
+        if (firstOperand == "") {
+            return;
+        } else {
+            operator = button.value;
+        }
+    })
+})
+
+equalBtn.addEventListener('click',()=>{
+    
+    switch (operator) {
+        case "plus":
+            const plusResult = parseInt(firstOperand) + parseInt(secondOperand);
+            screenNum.innerText = plusResult;
+            break;
+        case "minus":
+            const minusResult = parseInt(firstOperand) - parseInt(secondOperand);
+            screenNum.innerText = minusResult;
+            break;
+        case "multiply":
+            const multiplyResult = parseInt(firstOperand) * parseInt(secondOperand);
+            screenNum.innerText = multiplyResult;
+            break;
+        default:
+            const divideResult = parseInt(firstOperand) / parseInt(secondOperand);
+            screenNum.innerText = divideResult;
+    }
+})
+
+clearBtn.addEventListener('click',()=>{
+
+    screenNum.innerText = "0";
+    firstOperand = "";
+    secondOperand = "";
+    operator = ""
+
 })
