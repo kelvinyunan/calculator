fe4d030e2a9951af8c8edd4fb4e84dbf865b0fb8
Kelvin <121232509+kelvinyunan@users.noreply.github.com>
2024-06-21 18:15:18 +0700

    all functions working, maybe few uncaught bugs left

--- javascript.js
+++ javascript.js
@@ -7,6 +7,7 @@ const equalBtn = document.getElementById('equalBtn');
 const decimalBtn = document.getElementById('decimalBtn');
 const screenNum = document.getElementById('screen');
 
+let resetter = false;
 let firstOperand = "";
 let secondOperand = "";
 let operator = "";
@@ -14,18 +15,26 @@ let operator = "";
 numBtn.forEach((button)=>{
     button.addEventListener('click',()=>{
 
-        if (firstOperand == "") {
-            firstOperand = button.value.toString();
-            screenNum.textContent = firstOperand;
-        } else if (firstOperand != null && operator == "") {
-            firstOperand += button.value.toString();
-            screenNum.textContent = firstOperand;
-        } else if (secondOperand == "" && operator != "") {
-            secondOperand = button.value.toString();
-            screenNum.textContent = secondOperand;
+        if (firstOperand.length == 10 || secondOperand.length == 10) {
+            return;
         } else {
-            secondOperand += button.value.toString();
-            screenNum.textContent = secondOperand;
+            if (firstOperand == "" && resetter == false) {
+                firstOperand = button.value.toString();
+                screenNum.textContent = firstOperand;
+            } else if (firstOperand != "" && resetter == true && operator == "") {          //
+                firstOperand = button.value.toString();                                     //
+                screenNum.textContent = firstOperand;                                       //
+                resetter = false;                                                           //
+            } else if (firstOperand != "" && operator == "") {
+                firstOperand += button.value.toString();
+                screenNum.textContent = firstOperand;
+            } else if (secondOperand == "" && operator != "") {
+                secondOperand = button.value.toString();
+                screenNum.textContent = secondOperand;
+            } else {
+                secondOperand += button.value.toString();
+                screenNum.textContent = secondOperand;
+            };
         }
 
     })
@@ -36,30 +45,58 @@ operatorBtn.forEach((button)=>{
 
         if (firstOperand == "") {
             return;
-        } else {
+        } else if (firstOperand != "" && operator != "" && secondOperand != "") {
+            operation();
+            firstOperand = screenNum.innerText;
+            operator = button.value;
+            secondOperand = ""
+        }
+        else {
             operator = button.value;
         }
     })
 })
 
-equalBtn.addEventListener('click',()=>{
-    
+function operation() {
+
     switch (operator) {
         case "plus":
-            const plusResult = parseInt(firstOperand) + parseInt(secondOperand);
+            resetter = false;
+            const plusResult = Number(firstOperand) + Number(secondOperand);
             screenNum.innerText = plusResult;
+            newCalc(plusResult);
             break;
         case "minus":
-            const minusResult = parseInt(firstOperand) - parseInt(secondOperand);
+            resetter = false;
+            const minusResult = Number(firstOperand) - Number(secondOperand);
             screenNum.innerText = minusResult;
+            newCalc(minusResult);
             break;
         case "multiply":
-            const multiplyResult = parseInt(firstOperand) * parseInt(secondOperand);
+            resetter = false;
+            const multiplyResult = Number(firstOperand) * Number(secondOperand);
             screenNum.innerText = multiplyResult;
+            newCalc(multiplyResult);
             break;
         default:
-            const divideResult = parseInt(firstOperand) / parseInt(secondOperand);
-            screenNum.innerText = divideResult;
+            if (secondOperand == "0") {
+                screenNum.innerHTML = "Bruh";
+            } else {
+                resetter = false;
+                const divideResult = Number(firstOperand) / Number(secondOperand);
+                screenNum.innerText = divideResult;
+                newCalc(divideResult)
+            }
+    }
+}
+
+equalBtn.addEventListener('click',()=>{
+    
+    if (firstOperand == "" || operator == "" || secondOperand == "") {
+        return;
+    } else {
+    operation();
+    resetter =  true;
     }
 })
 
@@ -69,5 +106,90 @@ clearBtn.addEventListener('click',()=>{
     firstOperand = "";
     secondOperand = "";
     operator = ""
+    resetter = false;
 
 })
+
+negativeBtn.addEventListener('click',()=>{
+
+    if (firstOperand == "" && operator == "" && secondOperand == "") {
+        return;
+    };
+    if (firstOperand != "" && operator == "" && secondOperand == "") {
+        if (firstOperand.toString().includes('-')) {
+            const tempHolder = firstOperand.replace('-',"");
+            firstOperand = tempHolder;
+            screenNum.textContent = firstOperand;
+        } else {
+            firstOperand = '-' + firstOperand;
+            screenNum.textContent = firstOperand;
+        }
+    };
+    if (firstOperand != "" && operator != "" && secondOperand == "") {
+        return;
+    }
+    if (firstOperand != "" && operator != "" && secondOperand != "") {
+        if (secondOperand.toString().includes('-')) {
+            const tempHolder = secondOperand.replace('-',"");
+            secondOperand = tempHolder;
+            screenNum.textContent = secondOperand
+        } else {
+            secondOperand = '-' + secondOperand;
+            screenNum.textContent = secondOperand;
+        }
+    }
+})
+
+percentBtn.addEventListener('click',()=>{
+
+    if (firstOperand == "" && operator == "" && secondOperand == "") {
+        return;
+    };
+    if (firstOperand != "" && operator == "" && secondOperand == "") {
+        const tempHolder = Number(firstOperand) / 100;
+        firstOperand = tempHolder;
+        screenNum.textContent = firstOperand;
+    };
+    if (firstOperand != "" && operator != "" && secondOperand == "") {
+        return;
+    }
+    if (firstOperand != "" && operator != "" && secondOperand != "") {
+        const tempHolder = Number(secondOperand) / 100;
+        secondOperand = tempHolder;
+        screenNum.textContent = secondOperand;
+    }
+
+})
+
+function newCalc(prevResult) {
+    firstOperand = prevResult
+    operator = "";
+    secondOperand = "";
+}
+
+decimalBtn.addEventListener('click',()=>{
+
+    if (screenNum.innerText.includes('.')) {
+        return;
+    } else {
+        if (firstOperand.length == 10 || secondOperand.length == 10) {
+            return;
+        } else {
+            if (firstOperand == "") {
+                firstOperand = "0."
+                screenNum.textContent = firstOperand;
+            } else if (firstOperand != "" && operator == "" && secondOperand == "") {
+                firstOperand += '.';
+                screenNum.textContent = firstOperand;
+            } else if (firstOperand != "" && operator != "" && secondOperand == "") {
+                secondOperand = "0."
+                screenNum.textContent = secondOperand;
+            } else if (firstOperand != "" && operator != "" && secondOperand != "") {
+                secondOperand += '.';
+                screenNum.textContent = secondOperand;
+            };
+        }
+    }
+
+}
+)
